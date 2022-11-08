d8b852c5ca413c2e3dfbb6abd0faeee7375b6410
Audrey Washington-Wright <98628595+AudreyWash@users.noreply.github.com>
2022-11-08 12:05:08 -0600

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -9,4 +9,8 @@ if len(sys.argv) == 2:
 else:
     print("Invalid amount of arguments.")
     print("Syntax: python3 scanner.py <ip>")
+    
+print("-" * 50)
+print("Scanning target " + target)
+print("Time started: " + str(datetime.now()))
 
