ae52d6900d490840f226d62e82ccd5265143111c
Audrey Washington-Wright <98628595+AudreyWash@users.noreply.github.com>
2022-11-08 12:00:47 -0600

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -6,4 +6,7 @@ from datetime import datetime
 
 if len(sys.argv) == 2:
     target = socket.gethostbyname(sys.argv[1]) #Translate hostname to IPv4
+else:
+    print("Invalid amount of arguments.")
+    print("Syntax: python3 scanner.py <ip>")
 
